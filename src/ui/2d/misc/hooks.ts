be1b63444ad84301061746924d4dd18866c23be0
Yuan Wang <WANGYUAN@jp.ibm.com>
2025-01-13 16:40:59 +0900

    added necessary hook

---
+++ src/ui/2d/misc/hooks.ts
@@ -0,0 +1,28 @@
+import { useCallback, useEffect, useRef } from "react"
+
+const useAnimationFrame = (callback: (deltaTime: number) => void) => {
+  const requestRef = useRef<number>()
+  const previousTimeRef = useRef<number>()
+
+  const animate = useCallback((time: number) => {
+    if (previousTimeRef.current !== undefined) {
+      const deltaTime = time - previousTimeRef.current
+      callback(deltaTime)
+    }
+    previousTimeRef.current = time
+    requestRef.current = requestAnimationFrame(animate)
+  }, [callback])
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate)
+    return () => {
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current)
+      }
+    }
+  }, [animate])
+}
+
+export {
+  useAnimationFrame,
+}
